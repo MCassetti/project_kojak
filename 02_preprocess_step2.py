0db1be98a3b303a2480069bda3b504d52d64c0b4
MCassetti <cassetti.martha@gmail.com>
2018-09-08 13:53:02 -0700

    adding the meme caption preprocessing

---
+++ 02_preprocess_step2.py
@@ -0,0 +1,6 @@
+import os
+
+
+current_dir = os.getcwd()
+data_dir = '/meme_downloads/'
+meme_caption_path = current_dir + data_dir
--- 03_train_dankly.py
+++ 03_train_dankly.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'.format(epoch, num_epochs, i, total_step, loss.item(), np.exp(loss.item())))
 
             # Save the model checkpoints
-            #if (i+1) % save_step == 0:
-            torch.save(decoder.state_dict(), os.path.join(
-                model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-            torch.save(encoder.state_dict(), os.path.join(model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+            if (i+1) % save_step == 0:
+                torch.save(decoder.state_dict(), os.path.join(
+                    model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+                torch.save(encoder.state_dict(), os.path.join(model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
