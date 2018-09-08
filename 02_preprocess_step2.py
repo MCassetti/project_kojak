047779c7ca3ed3559be271e513c03fbda14f4aed
MCassetti <cassetti.martha@gmail.com>
2018-09-08 15:12:12 -0700

    caption dictionary created...down sample this shit please

--- 02_preprocess_step2.py
+++ 02_preprocess_step2.py
@@ -19,26 +19,37 @@ for meme in meme_text:
 
     with open(meme_caption_path + meme,'rb') as f:
         for line in f:
-            curr_caption = []
-            current_line = line.decode(errors='ignore').strip('\n').split('\t')
-            tag = current_line[0].replace('top caption:','').replace('\"','')
-            top_caption = current_line[1].replace('top caption:','').replace('\"','')
-            bottom_caption = current_line[2].replace('bottom_caption:','').replace('\"','')
-            caption = top_caption.lstrip() + pause_token + bottom_caption.lstrip()
+            try:
 
-            image = tag + '.jpg'
-            caption_dict['image_id'] = image_id
-            caption_dict['caption'] = caption
-            caption_dict['id'] = id
-            image_dict['file_name'] = image
-            image_dict['image_id'] = image_id
-            print(caption_dict, image_dict)
-            caption_list.append(caption_dict)
-            image_list.append(image_dict)
-            id += 1
+                current_line = line.decode(errors='ignore').strip('\n').split('\t')
+                tag = current_line[0].replace('top caption:','').replace('\"','')
+                top_caption = current_line[1].replace('top caption:','').replace('\"','')
+                bottom_caption = current_line[2].replace('bottom_caption:','').replace('\"','')
+                caption = top_caption.lstrip() + pause_token + bottom_caption.lstrip()
+
+                image = tag + '.jpg'
+                caption_dict['image_id'] = image_id
+                caption_dict['caption'] = caption
+                caption_dict['id'] = id
+                image_dict['file_name'] = image
+                image_dict['image_id'] = image_id
+
+                caption_list.append(caption_dict)
+                image_list.append(image_dict)
+                id += 1
+                if id % 100 == 0:
+                    print(caption,tag,image)
+            except:
+                "skipping caption, split did not work..."
+                continue
 
         image_id +=1 # image ids
 
 full_caption_dict['info'] = info_dict
 full_caption_dict['captions'] = caption_list
 full_caption_dict['images'] = image_list
+
+
+
+with open('captions.json', 'w') as fp:
+    json.dump(full_caption_dict, fp)
